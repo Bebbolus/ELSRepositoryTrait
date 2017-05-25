54db849242d596e833bb3f058e7a185b8ca69df4
Roberto <lambis.net@gmail.com>
2017-05-25 10:37:36 +0200

    change kind of info on EDM_DELETED

--- src/repositoryTrait.php
+++ src/repositoryTrait.php
@@ -83,7 +83,7 @@ trait RepositoryTrait
     
     public function getActive($conditions = [], $requiredField = [], $page = 0)
     {
-        $conditions['EDM_DELETED']=false;
+        $conditions['EDM_DELETED']=0;
         return $this->repo->get($conditions, $requiredField, $page);
     }
 
@@ -99,7 +99,7 @@ trait RepositoryTrait
     
     public function countAcive($conditions = [], $requiredField = [])
     {
-        $conditions['EDM_DELETED']=false;
+        $conditions['EDM_DELETED']=0;
         return $this->repo->count($conditions, $requiredField);
     }
 
@@ -109,13 +109,13 @@ trait RepositoryTrait
 
     public function indexWithId($content)
     {
-        $content['EDM_DELETED']=false;
+        $content['EDM_DELETED']=0;
         return $this->repo->indexWithId($this->getElsId(), $content);
     }
 
     public function index($content)
     {
-        $content['EDM_DELETED']=false;
+        $content['EDM_DELETED']=0;
         return $this->repo->index($content);
     }
 
@@ -150,7 +150,7 @@ trait RepositoryTrait
     
     public function softDelete()
     {
-        $this->setAttributes('EDM_DELETED',true);
+        $this->setAttributes('EDM_DELETED',0);
         return $this->save();
     }
 
